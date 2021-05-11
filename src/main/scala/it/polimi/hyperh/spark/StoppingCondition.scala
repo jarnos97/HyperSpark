3412a40448f1988ec21517c603c47a1d13d427ac
Jarno Smit <51162363+jarnos97@users.noreply.github.com>
2021-05-11 14:32:47 +0200

    Added stopping condition: TemperatureLimit.scala

--- src/main/scala/it/polimi/hyperh/spark/StoppingCondition.scala
+++ src/main/scala/it/polimi/hyperh/spark/StoppingCondition.scala
@@ -9,11 +9,16 @@ import java.lang.management.ThreadMXBean;
 abstract class StoppingCondition extends Serializable {
   def isSatisfied(): Boolean
   def isNotSatisfied(): Boolean = { ! isSatisfied() }
+
+  // Added additional versions to base class which accept parameters
+  def isSatisfiedParam(param: Double): Boolean
+//  def isNotSatisfiedParam: Boolean = { ! isSatisfiedParam()}
+
 }
 
 class TimeExpired(timeLimitMillis: Double) extends StoppingCondition {
   private var internalLimit: Double = 9999999999999999.0
-  
+
   private def getThreadTime(): Long = {
     val threadTimeNanos = ManagementFactory.getThreadMXBean().getThreadCpuTime(Thread.currentThread().getId());
     val threadTimeMillis = threadTimeNanos / 1000000
@@ -31,5 +36,9 @@ class TimeExpired(timeLimitMillis: Double) extends StoppingCondition {
       true
     else false
   }
+
+  // Empty override
+  override def isSatisfiedParam(param: Double): Boolean = {false}
+
   def getLimit(): Double = { timeLimitMillis }
 }
